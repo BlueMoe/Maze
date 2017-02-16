26a646520e987ab5e7745b1bf4d24c9ea7a6cb37
BlueWhiteSoul <matrixofprime@gmail.com>
2017-02-16 15:53:22 +0800

    create project

---
+++ Assets/Scripts/teleportation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class teleportation : MonoBehaviour
+{
+    public Material phantomShader;
+    public Material normalShader;
+    public GameObject Ambra;
+    private float _teleportationSpeed = 5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            GameObject phantom = Instantiate(Ambra, transform.position, transform.rotation);
+            phantom.GetComponentInChildren<Renderer>().material = phantomShader;
+            phantom.GetComponent<Animator>().Stop();
+            phantom.GetComponent<ThirdPersonUserControl>().enabled = false;
+            phantom.GetComponent<teleportation>().enabled = false;
+            phantom.GetComponent<Rigidbody>().isKinematic = true;
+            phantom.GetComponent<CapsuleCollider>().enabled = false;
+            Ambra.transform.Translate(Vector3.forward * _teleportationSpeed);
+        }
+    }
+}
