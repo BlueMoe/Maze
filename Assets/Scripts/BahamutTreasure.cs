875ffbdd90ed832708d0c3c06912cf4fe74642a2
BlueWhiteSoul <matrixofprime@gmail.com>
2017-02-22 17:51:31 +0800

    bahamut model script

---
+++ Assets/Scripts/BahamutTreasure.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BahamutTreasure : MonoBehaviour {
+
+    public GameObject Ambra;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Ambra == null) return;
+        if (collision.gameObject != Ambra) return;
+        
+        Destroy(gameObject);
+    }
+}
