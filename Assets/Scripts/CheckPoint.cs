9bfd789b3ad4696eab415b45ebc80b8d53cd3a42
BlueWhiteSoul <matrixofprime@gmail.com>
2017-02-23 17:33:25 +0800

    checkpoint will mark index when Ambra Trigger it.

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    public int Index = 0;
+
+	void OnTriggerEnter(Collider collider)
+    {
+        var deadController = collider.gameObject.GetComponent<DeadController>();
+        if(deadController)
+        {
+            deadController.setCheckPointIndex(Index);
+        }
+    }
+}
