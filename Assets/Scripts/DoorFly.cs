33118b697cb89b79e765d45fcc1bad91cd824e37
bluemoe <matrixofprime@gmail.com>
2017-03-12 12:18:27 +0800

    3rd room to 4th room

---
+++ Assets/Scripts/DoorFly.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorFly : MonoBehaviour {
+
+    private float _force = 75;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void DoActivateTrigger()
+    {
+        fly();
+    }
+
+    void fly()
+    {
+        transform.Translate(new Vector3(-2, 0, 0));
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GetComponent<Rigidbody>().AddForce(new Vector3(1, 1, 1) * _force,ForceMode.VelocityChange);
+    }
+}
